7f1339503af822fdc6f1cecbc3fe08ef61e17d37
Octoping <myc1365@naver.com>
2022-07-16 16:33:45 +0900

    STYLE: 들여쓰기 공백 2개로 수정

--- js/answer.js
+++ js/answer.js
@@ -1,24 +1,24 @@
 import {CONSTANTS} from "./config.js";
 
 export class Answer {
-    word;
+  word;
 
-    constructor(word) {
-        this.word = word;
-    }
+  constructor(word) {
+    this.word = word;
+  }
 
-    isEqual(word) {
-        return this.word === word;
-    }
+  isEqual(word) {
+    return this.word === word;
+  }
 
-    getColor(char, idx) {
-        switch(true) {
-            case this.word[idx] === char:
-                return CONSTANTS.GREEN;
-            case this.word.includes(char):
-                return CONSTANTS.YELLOW;
-            default:
-                return CONSTANTS.GRAY;
-        }
+  getColor(char, idx) {
+    switch(true) {
+      case this.word[idx] === char:
+        return CONSTANTS.GREEN;
+      case this.word.includes(char):
+        return CONSTANTS.YELLOW;
+      default:
+        return CONSTANTS.GRAY;
     }
+  }
 }
--- js/session.js
+++ js/session.js
@@ -2,51 +2,51 @@ import {CONSTANTS} from "./config.js";
 import {Answer} from "./answer.js";
 
 export class Session {
-    answer;
-    leftTryCount;
-    submittedWord;
-    submittedResult;
+  answer;
+  leftTryCount;
+  submittedWord;
+  submittedResult;
 
-    constructor(answer) {
-        this.answer = new Answer(answer);
-        this.leftTryCount = CONSTANTS.TRY_COUNT;
-        this.submittedWord = [];
-        this.submittedResult = [];
-    }
+  constructor(answer) {
+    this.answer = new Answer(answer);
+    this.leftTryCount = CONSTANTS.TRY_COUNT;
+    this.submittedWord = [];
+    this.submittedResult = [];
+  }
 
-    submitAnswer(inputWord) {
-        this.submittedWord.push(inputWord);
-        this.submittedResult.push(this.getColorArray(inputWord));
-        --this.leftTryCount;
-    }
+  submitAnswer(inputWord) {
+    this.submittedWord.push(inputWord);
+    this.submittedResult.push(this.getColorArray(inputWord));
+    --this.leftTryCount;
+  }
 
-    getColorArray(inputWord) {
-        return inputWord.split('').map((x, idx) => this.answer.getColor(x, idx));
-    }
+  getColorArray(inputWord) {
+    return inputWord.split('').map((x, idx) => this.answer.getColor(x, idx));
+  }
 
-    isAnswer(inputWord) {
-        return this.answer.isEqual(inputWord);
-    }
+  isAnswer(inputWord) {
+    return this.answer.isEqual(inputWord);
+  }
 
-    getWordleTableHTML() {
-        let tableHTML = '<table>';
-        for(let i = 0; i < CONSTANTS.TRY_COUNT; ++i) {
-            tableHTML += '<tr>';
-            for(let j = 0; j < CONSTANTS.WORD_LEN; ++j) {
-                if(i < this.submittedWord.length) {
-                    tableHTML += `<td class=${CONSTANTS.TD_TYPE[this.submittedResult[i][j]]}>${this.submittedWord[i][j]}</td>`;
-                }
-                else {
-                    tableHTML += `<td></td>`;
-                }
-            }
-            tableHTML += '</tr>';
+  getWordleTableHTML() {
+    let tableHTML = '<table>';
+    for(let i = 0; i < CONSTANTS.TRY_COUNT; ++i) {
+      tableHTML += '<tr>';
+      for(let j = 0; j < CONSTANTS.WORD_LEN; ++j) {
+        if(i < this.submittedWord.length) {
+          tableHTML += `<td class=${CONSTANTS.TD_TYPE[this.submittedResult[i][j]]}>${this.submittedWord[i][j]}</td>`;
         }
-        tableHTML += '</table>';
-        return tableHTML;
+        else {
+          tableHTML += `<td></td>`;
+        }
+      }
+      tableHTML += '</tr>';
     }
+    tableHTML += '</table>';
+    return tableHTML;
+  }
 
-    getResultStr() {
-        return this.submittedResult.map(submitResult => submitResult.map(x => CONSTANTS.ICON[x]).join('')).join('\n');
-    }
+  getResultStr() {
+    return this.submittedResult.map(submitResult => submitResult.map(x => CONSTANTS.ICON[x]).join('')).join('\n');
+  }
 }
